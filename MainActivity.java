d957bff88d6ddba2f5d00d35b45d69eb52c12500
DanutaGagua <49496698+DanutaGagua@users.noreply.github.com>
2019-11-10 13:44:56 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.personalfilmcollectionmanager;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void signIn(View view)
+    {
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
+    }
+
+    public void signUp(View view)
+    {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
+    public void signInAsVisitor(View view)
+    {
+        Intent intent = new Intent(this, FindFilmActivity.class);
+        startActivity(intent);
+    }
+}
