ae3efd8045091f17e25ee65b56612135e17a502a
DanutaGagua <49496698+DanutaGagua@users.noreply.github.com>
2019-11-10 14:59:25 +0300

    Add files via upload

---
+++ code/java classes/Film.java
@@ -0,0 +1,34 @@
+package com.example.personalfilmcollectionmanager;
+
+public class Film {
+    String name;
+    String year;
+    String releaseDate;
+    String country;
+    String budget;
+    String cumulativeWorldwideGross;
+    String runtime;
+    String userFeedback = "";
+
+    public Film(String name,
+                String year,
+                String releaseDate,
+                String country,
+                String budget,
+                String cumulativeWorldwideGross,
+                String runtime)
+    {
+        this.name = name;
+        this.year = year;
+        this.releaseDate = releaseDate;
+        this.country = country;
+        this.budget = budget;
+        this.cumulativeWorldwideGross = cumulativeWorldwideGross;
+        this.runtime = runtime;
+    }
+
+    public void setUserFeedback(String userFeedback)
+    {
+        this.userFeedback = userFeedback;
+    }
+}
