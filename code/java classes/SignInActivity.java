bccb7484877547d8714c54f5451f07b57c77c0aa
DanutaGagua <49496698+DanutaGagua@users.noreply.github.com>
2019-11-16 18:47:00 +0300

    Update SignInActivity.java

--- code/java classes/SignInActivity.java
+++ code/java classes/SignInActivity.java
@@ -1,12 +1,38 @@
 package com.example.personalfilmcollectionmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 public class SignInActivity extends AppCompatActivity {
+    String[] cities = {"Москва", "Самара", "Вологда", "Волгоград", "Саратов", "Воронеж"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Spinner spinner = (Spinner) findViewById(R.id.users);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, cities);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(0);
+    }
+
+    public void signIn(View view)
+    {
+        //Intent intent = new Intent(this, SignInActivity.class);
+        //startActivity(intent);
+    }
+
+    public void cancel(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
     }
 }
