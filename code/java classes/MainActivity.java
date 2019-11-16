b4ca8a036a1d5dbeced36257bd0597381bc61ef9
DanutaGagua <49496698+DanutaGagua@users.noreply.github.com>
2019-11-16 19:41:28 +0300

    Update MainActivity.java

--- code/java classes/MainActivity.java
+++ code/java classes/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     boolean visitor = false;
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setHelpTextView();
     }
 
     public void signIn(View view)
@@ -31,7 +34,13 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, FindFilmActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         intent.putExtra("visitorFlag", visitor);
-        
+
         startActivity(intent);
+    }
+
+    private void setHelpTextView()
+    {
+        TextView textView = findViewById(R.id.help_text_view);
+        textView.setText("Help:\nIf you want to use application without signing up click button \"Sign in as visitor\"");
     }
 }
