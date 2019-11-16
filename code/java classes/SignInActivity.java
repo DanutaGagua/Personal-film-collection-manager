3e0a9caaef9617d262f6789dee9481055de24871
DanutaGagua <49496698+DanutaGagua@users.noreply.github.com>
2019-11-16 18:54:52 +0300

    Update SignInActivity.java

--- code/java classes/SignInActivity.java
+++ code/java classes/SignInActivity.java
@@ -3,18 +3,21 @@ package com.example.personalfilmcollectionmanager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Layout;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 public class SignInActivity extends AppCompatActivity {
-    String[] cities = {"Москва", "Самара", "Вологда", "Волгоград", "Саратов", "Воронеж"};
+    private final static String FILE_NAME = "content.txt";
+    
+    String[] cities = {"Lady", "Bob", "Ann"};
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        
+        ;//Us user = etUserList
 
         Spinner spinner = (Spinner) findViewById(R.id.users);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, cities);
