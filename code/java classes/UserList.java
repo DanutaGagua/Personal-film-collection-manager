1d0c5b961bc409162a1c3f17cc109fe835773d2c
DanutaGagua <49496698+DanutaGagua@users.noreply.github.com>
2019-11-19 22:19:42 +0300

    Update UserList.java

--- code/java classes/UserList.java
+++ code/java classes/UserList.java
@@ -5,44 +5,49 @@ import java.util.ArrayList;
 public class UserList {
     ArrayList<User> userList;
 
-    public UserList()
-    {
+    public UserList() {
         userList = new ArrayList<>();
     }
 
-    public boolean addUser(User user)
-    {
-        if (userList.contains(user))
-        {
+    public boolean addUser(User user) {
+        if (userList.contains(user)) {
             return false;
-        }
-        else
-        {
+        } else {
             userList.add(user);
 
             return true;
         }
     }
 
-    public void deleteUser(User user)
-    {
+    public void deleteUser(User user) {
         userList.remove(user);
     }
 
-    public int getUserNumber()
-    {
+    public int getUserNumber() {
         return userList.size();
     }
 
-    public String[] getUserNames()
-    {
+    public String[] getUserNames() {
         String[] names = new String[userList.size()];
 
-        for (User user: userList)
-        {
+        for (User user: userList) {
             names[userList.indexOf(user)] = user.name;
         }
 
         return names;
+    }
+
+    public User findUser(String name) {
+        User user = new User();
+
+        for (User userIndex: userList) {
+            if (userIndex.name.equals(name)) {
+                user = userIndex;
+                
+                break;
+            }
+        }
+
+        return user;
     }
 }
