45eda20753914d22fb9d8ef9490a839d94ac8776
DanutaGagua <49496698+DanutaGagua@users.noreply.github.com>
2019-11-21 01:15:15 +0300

    Update Film.java

--- code/java classes/Film.java
+++ code/java classes/Film.java
@@ -1,14 +1,14 @@
 package com.example.personalfilmcollectionmanager;
 
 public class Film {
-    String name;
-    String year; 
-    String releaseDate;
-    String country;
-    String budget;
-    String cumulativeWorldwideGross;
-    String runtime;
-    String userFeedback = "";
+    private String name;
+    private String year;
+    private String releaseDate;
+    private String country;
+    private String budget;
+    private String cumulativeWorldwideGross;
+    private String runtime;
+    private String userFeedback = "";
 
     public Film(String name,
                 String year,
