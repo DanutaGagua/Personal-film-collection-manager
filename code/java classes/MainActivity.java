da7af24fa68cde4cbfc0e58baea55940440fca4c
DanutaGagua <49496698+DanutaGagua@users.noreply.github.com>
2019-11-21 01:33:52 +0300

    Update MainActivity.java

--- code/java classes/MainActivity.java
+++ code/java classes/MainActivity.java
@@ -1,47 +1,67 @@
 package com.example.personalfilmcollectionmanager;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
+    private String previousClassName = "";
+    private UserList userList;
     private boolean visitor = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
 
-        setHelpTextView();
-    }
+        Bundle arguments = getIntent().getExtras();
+        previousClassName = arguments.getString("previousClassName");
+        visitor = arguments.getBoolean("visitorFlag");
 
-    public void signIn(View view) {
-        startActivity(new Intent(this, SignInActivity.class));
+        TextView textView = findViewById(R.id.activity_sign_in_info);
+        textView.setText("Enter username and \n add new user");
     }
 
     public void signUp(View view) {
-        Intent intent = new Intent(this, SignUpActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        intent.putExtra("visitorFlag", visitor);
-        intent.putExtra("previousClassName", "MainActivity");
+        //if ()
 
+        //Intent intent = new Intent(this, SignInActivity.class);
+        //startActivity(intent);
+    }
+
+    public void cancel(View view) {
+        Intent intent;
+
+        if (previousClassName.equals("MainActivity")) {
+            intent = new Intent(this, MainActivity.class);
+        } else if (previousClassName.equals("FindFilmActivity")) {
+            intent = new Intent(this, FindFilmActivity.class);
+        } else {
+            intent = new Intent(this, ShowFilmInformationActivity.class);
+        }
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        intent.putExtra("visitorFlag", visitor);
         startActivity(intent);
     }
 
-    public void signInAsVisitor(View view) {
-        visitor = true;
+    private UserList getUserList() {
+        UserList userList = new UserList();
 
-        Intent intent = new Intent(this, FindFilmActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        intent.putExtra("visitorFlag", visitor);
+        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("users.db", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS users (name TEXT)");
+        Cursor query = db.rawQuery("SELECT * FROM users;", null);
 
-        startActivity(intent);
-    }
+        if(query.moveToFirst()) {
+            String name = query.getString(0);
 
-    private void setHelpTextView() {
-        TextView textView = findViewById(R.id.help_text_view);
-        textView.setText("Help:\nIf you want to use application without signing up click button \"Sign in as visitor\"");
+            userList.addUser(new User(name));
+        }
+
+        return userList;
     }
 }
