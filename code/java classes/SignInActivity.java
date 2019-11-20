9fdc22c1695e3fa1ed7d4160711187c3a5cb4d1d
DanutaGagua <49496698+DanutaGagua@users.noreply.github.com>
2019-11-21 01:19:53 +0300

    Update SignInActivity.java

--- code/java classes/SignInActivity.java
+++ code/java classes/SignInActivity.java
@@ -9,10 +9,9 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class SignInActivity extends AppCompatActivity {
-    UserList userList;
+    private UserList userList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +24,16 @@ public class SignInActivity extends AppCompatActivity {
 
         String[] users = userList.getUserNames();
 
-        Spinner spinner = (Spinner) findViewById(R.id.users);
+        Spinner spinner = findViewById(R.id.users);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, users);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setSelection(0);
     }
 
-    public void signIn(View view)
-    {
-        if (userList.getUserNumber() != 0)
-        {
-            Spinner spinner = (Spinner) findViewById(R.id.users);
+    public void signIn(View view) {
+        if (userList.getUserNumber() != 0) {
+            Spinner spinner = findViewById(R.id.users);
             String name = spinner.getSelectedItem().toString();
 
             User user = userList.findUser(name);
@@ -47,22 +44,20 @@ public class SignInActivity extends AppCompatActivity {
         }
     }
 
-    public void cancel(View view)
-    {
+    public void cancel(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         startActivity(intent);
     }
 
-    private UserList getUserList()
-    {
+    private UserList getUserList() {
         UserList userList = new UserList();
 
         SQLiteDatabase db = getBaseContext().openOrCreateDatabase("users.db", MODE_PRIVATE, null);
         db.execSQL("CREATE TABLE IF NOT EXISTS users (name TEXT)");
         Cursor query = db.rawQuery("SELECT * FROM users;", null);
 
-        if(query.moveToFirst()){
+        if(query.moveToFirst()) {
             String name = query.getString(0);
 
             userList.addUser(new User(name));
@@ -71,16 +66,12 @@ public class SignInActivity extends AppCompatActivity {
         return userList;
     }
 
-    private void setInfo()
-    {
+    private void setInfo() {
         TextView textView = findViewById(R.id.activity_sign_in_info);
 
-        if (userList.getUserNumber() == 0)
-        {
+        if (userList.getUserNumber() == 0) {
             textView.setText("There are not users in list");
-        }
-        else
-        {
+        } else {
             textView.setText("Select your username \n and sign in");
         }
     }
